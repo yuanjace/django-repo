e8dbfda841b11feebc5246c7ccb6d2266629dd4d
yuanjace <>
2024-12-18 11:39:51 +0800

    init

---
+++ todo/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import TodoViewSet
+
+# 創建一個 DefaultRouter 並註冊 TodoViewSet
+router = DefaultRouter()
+router.register(r'todos', TodoViewSet)  # 確保這裡是 'todos'
+
+urlpatterns = [
+    path('', include(router.urls)),  # 把 router 的 urls 包含進來
+]
