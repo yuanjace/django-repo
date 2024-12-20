0a1cb2a74c14129822e9be771ab98521810aef1b
yuanjace <>
2024-12-20 11:51:52 +0800

    Add Throttled
    限制API頻率

---
+++ todo/throttles.py
@@ -0,0 +1,26 @@
+# throttles.py
+from gettext import ngettext
+import math
+from black import format_str
+from rest_framework.throttling import BaseThrottle
+from rest_framework.exceptions import Throttled
+
+from datetime import datetime, timedelta
+from django.core.cache import cache
+
+class CustomThrottled(Throttled):
+    default_detail = '好了啦，API不要打這麼急躁。慢慢來比較快'
+
+class FiveSecondThrottle(BaseThrottle):
+    def allow_request(self, request, view):
+        key = f"request_{request.user.id}" if request.user.is_authenticated else f"request_{request.META['REMOTE_ADDR']}"
+        last_request_time = cache.get(key)
+
+        if not last_request_time or datetime.now() - last_request_time > timedelta(seconds=5):
+            cache.set(key, datetime.now(), timeout=5)  # 設置下一次請求的有效期為5秒
+            return True
+
+        raise CustomThrottled()
+    
+
+
--- todo/views.py
+++ todo/views.py
@@ -3,15 +3,18 @@ from rest_framework.pagination import PageNumberPagination
 
 from .models import Todo
 from .serializers import TodoSerializer
+from .throttles import FiveSecondThrottle
 
 # 自定義分頁器
 class TodoPagination(PageNumberPagination):
-    page_size = 20  # 每一頁顯示 10 項
+    page_size = 10  # 每一頁顯示 10 項
     page_size_query_param = 'page_size'  # 允許客戶端通過這個參數設置每頁的項目數
     max_page_size = 100  # 設置每頁的最大項目數
 
 
 class TodoViewSet(viewsets.ModelViewSet):
+    throttle_classes = [FiveSecondThrottle]
+
     queryset = Todo.objects.all().order_by('-created_at')
     serializer_class = TodoSerializer
     pagination_class = TodoPagination  
